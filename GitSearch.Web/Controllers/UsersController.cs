d581cea36a2f4271163f5426d2a1b2f0f9d2e3c7
r3core <s3698496@student.rmit.edu.au>
2018-04-29 05:20:32 +1000

    Removed contact.

--- GitSearch.Web/Controllers/UsersController.cs
+++ GitSearch.Web/Controllers/UsersController.cs
@@ -67,13 +67,6 @@ namespace GitSearch.Web.Controllers
             return View(user);
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
